bccf920466d45be9d82f5e1403af5165980d37fd
stewart <stewartmorales129@gmail.com>
2025-11-25 22:53:21 -0600

    support spanish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,24 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  around_action :switch_locale
+
   add_flash_types :success
 
   def redirect_signed_in_user
     return unless authenticated?
 
     redirect_back(fallback_location: after_authentication_url)
+  end
+
+  private
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
 end
--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -1,4 +1,5 @@
 class PasswordsMailer < ApplicationMailer
+  default_url_options[:locale] = I18n.default_locale
   def reset(user)
     @user = user
     mail subject: 'Reset your password', to: user.email
